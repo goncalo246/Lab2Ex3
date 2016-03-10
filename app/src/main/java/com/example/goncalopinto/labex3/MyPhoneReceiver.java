5d28f9fd43842084bfd65ebf11806c5e814f636a
Goncalo Pinto <goncalopinto94@gmail.com>
2016-03-10 11:46:51 +0000

    Test

--- app/src/main/java/com/example/goncalopinto/labex3/MyPhoneReceiver.java
+++ app/src/main/java/com/example/goncalopinto/labex3/MyPhoneReceiver.java
@@ -1,7 +1,18 @@
 package com.example.goncalopinto.labex3;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
 /**
  * Created by Goncalo Pinto on 10/03/2016.
  */
-public class MyPhoneReceiver {
+public class MyPhoneReceiver extends BroadcastReceiver{
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //efcec
+
+    }
 }
