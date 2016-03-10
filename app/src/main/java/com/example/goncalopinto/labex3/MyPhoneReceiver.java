2bbef6ccd1c2df3e8b64d8b9348a30f451a5d9f5
Goncalo Pinto <goncalopinto94@gmail.com>
2016-03-10 12:15:09 +0000

    Final version

--- app/src/main/java/com/example/goncalopinto/labex3/MyPhoneReceiver.java
+++ app/src/main/java/com/example/goncalopinto/labex3/MyPhoneReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Goncalo Pinto on 10/03/2016.
@@ -15,13 +16,14 @@ public class MyPhoneReceiver extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
         String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
-        Log.w("msg","Phone state changed to " + state);
-        if (TelephonyManager.EXTRA_STATE_RINGING.equals(state))
-        {
-            String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-            Log.w("msg","Phone state changed to " + state);
+        System.out.println("---------------->>>>>>>>>>>>>>>>>>> Phone state changed to " + state);
 
-        }
+
+        if (TelephonyManager.EXTRA_STATE_RINGING.equals(state))
+       {
+           String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+           System.out.println("Phone state changed to " + state);
+       }
 
 
     }
